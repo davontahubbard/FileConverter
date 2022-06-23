f28838640a3ee7c957ebe1d52bccf6e0f72605ad
Renee Belk <renee@Renees-Air.lan>
2022-06-22 19:15:40 -0500

    navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Navbar.Brand href="#home">File Converters</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="me-auto">
+    <NavDropdown title="Converters" id="collasible-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <Nav.Link href="#contact-us">Contact Us</Nav.Link>
+    </Nav>
+    <Nav>
+      <Nav.Link href="#deets">Login</Nav.Link>
+      <Nav.Link eventKey={2} href="#memes">
+        Sign Up
+      </Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
     </div>
   );
 }
